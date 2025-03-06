908d36191cce52e18c6972a2371a8bb564248293
Megood1210 <xuanvinhle06@gmail.com>
2025-03-06 20:48:44 +0700

    Upload bài tập 2

---
+++ ss22ex2.js
@@ -0,0 +1,8 @@
+let numbers = [2,5,7,4,1,8,6];
+let a = +prompt("Nhap mot so");
+if (numbers.includes(a)) {
+    console.log("Bingo");
+} else {
+    console.log("Chúc bạn may mắn lần sau");
+}
+    
